d15f9161b65e85bd08fb52e237bf583d03ded62d
schmix <gerasimov.georgi@gmail.com>
1998-01-08 22:22:23 +0200

    test init commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en"> 
+<head> 
+<meta http-equiv="Content-Type" CONTENT="text/html; charset=UTF-8" /> 
+<meta http-equiv="imagetoolbar" content="no" /> 
+<meta name="author" content="GG" /> 
+<title>M2</title> 
+<link rel="stylesheet" type="text/css" href="mooflow_preview/MooFlow.css" /> 
+<script type="text/javascript" src="mootools-1.2-core.js"></script> 
+<script type="text/javascript" src="mootools-1.2-more.js"></script>
+<script type="text/javascript" src="mooflow_preview/MooFlow.js"></script> 
+<script type="text/javascript" src="mooflow_preview/MooFlowViewer.js"></script>  
+</head> 
+<body>
+<a class="fdbk_tab_left tipz"  id="fdbk_tab"  href="#"  title="Натиснете за да се върнете в началото::!">Home</a>
+<div id="msgCont" ></div>
+<div id="wrapper"> 
+<div id="content"> 
+	<div id="MooFlow"> 
+	</div> 
+</div>  
+</div> 
+<div id="titleCont"  class="titleTip">&nbsp;</div>
+</div>
+</body> 
+</html>
